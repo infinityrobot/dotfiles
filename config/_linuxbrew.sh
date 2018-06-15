dc9cf87f9bc97150f8b7c35d002198a570a51842
Ritchie Blair <ritchie@infinityrobot.com>
2018-06-15 17:56:07 +1000

    :bug: :penguin: Add sbin back to Linuxbrew PATH export

--- config/_linuxbrew.sh
+++ config/_linuxbrew.sh
@@ -1,7 +1,7 @@
 if [[ -d "$HOME/.linuxbrew" ]]; then
   export PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
 elif [[ -d "/home/linuxbrew/.linuxbrew" ]]; then
-  export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+  export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
   export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"
   export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"
 fi
