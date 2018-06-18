0c32684980e7233d9df525087d13ba39a114a24c
Ritchie Blair <ritchie@infinityrobot.com>
2018-06-18 15:38:24 +1000

    :bulb: Update Postgres aliases for Linuxbrew installs

--- aliases/postgres.sh
+++ aliases/postgres.sh
@@ -1,2 +1,7 @@
-alias pg_start="pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start"
-alias pg_stop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
+if [[ -d "$HOME/.linuxbrew" ]]; then
+  alias pg_start="pg_ctl start -D $HOME/.linuxbrew/var/postgres -l logfile"
+  alias pg_stop="pg_ctl stop -D $HOME/.linuxbrew/var/postgres"
+elif [[ -d "/home/linuxbrew/.linuxbrew" ]]; then
+  alias pg_start="pg_ctl start -D /home/linuxbrew/.linuxbrew/var/postgres -l logfile"
+  alias pg_stop="pg_ctl stop -D /home/linuxbrew/.linuxbrew/var/postgres"
+fi
