45226eac4967d299e0acae5656709ae156edaf12
Ritchie Blair <ritchie@infinityrobot.com>
2018-06-28 13:10:07 +1000

    :wrench: :gem: Set global Ruby version env variable in rbenv config

--- config/rbenv.sh
+++ config/rbenv.sh
@@ -1,5 +1,8 @@
 # Initialise rbenv.
 if [[ -d "$HOME/.rbenv" ]]; then
+  # Add rbenv to PATH & initialize.
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init - --no-rehash zsh)"
+  # Export helpful ruby related env variables.
+  export RUBY_GLOBAL_VERSION="$(rbenv global)"
 fi
