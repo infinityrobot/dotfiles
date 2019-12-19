b04568cc382bafae1c454854a4e4303439f833fe
Ritchie Cotton <ritchie@infinityrobot.com>
2019-12-20 09:15:30 +1100

    :wrench: :haircut: Add prune_local git alias

--- aliases/git.sh
+++ aliases/git.sh
@@ -1,1 +1,3 @@
 alias grs="git reset --soft HEAD~1"
+# https://stackoverflow.com/questions/13064613/how-to-prune-local-tracking-branches-that-do-not-exist-on-remote-anymore
+alias prune_local="git branch --merged master | grep -v '^[ *]*master$' | xargs git branch -d"
